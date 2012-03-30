0dfe6798da6838513ef15d059a6be893c7b4d2a2
Yi, EungJun <semtlenori@gmail.com>
2012-03-30 16:30:26 +0900

    Rename deflatedBlob to defaltedObject.

--- gitfs.js
+++ gitfs.js
@@ -73,11 +73,11 @@ var createObject = function(raw, callback) {
     var digest = this.sha1sum(raw);
     var self = this;
     this.deflate(raw, function(err, result) {
-        var deflatedBlob = result;
+        var deflatedObject = result;
         self.createObjectBucket(digest, function(err, bucketPath) {
             if (err) throw err;
             var treePath = path.join(bucketPath, digest.substr(2));
-            fs.writeFile(treePath, deflatedBlob, callback);
+            fs.writeFile(treePath, deflatedObject, callback);
         });
     });
 }
