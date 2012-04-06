ff72586d0e3142e5c0b224f1e4bdcf4206a3e2fe
Yi, EungJun <semtlenori@gmail.com>
2012-04-06 10:33:10 +0900

    Add wiki module.
    * Add wiki.init to initialize wiki.
    * Add wiki.writePage to write wikipage.
    * Add wiki.getPage to read a wikipage.

---
+++ wiki.js
@@ -0,0 +1,28 @@
+var gitfs = require('./gitfs');
+
+function init(callback) {
+    gitfs.init(callback);
+}
+
+function writePage(name, content, callback) {
+    var files = {};
+
+    files[name] = content;
+
+    var commit = {
+        files: files,
+        author: {name: 'Guest', mail: 'guest@n4wiki.com', timezone: '+0900'},
+        committer: {name: 'Guest', mail: 'guest@n4wiki.com', timezone: '+0900'},
+        message: 'Edit ' + name
+    };
+
+    gitfs.commit(commit, callback);
+}
+
+function getPage(name, callback) {
+    gitfs.show(name, callback);
+}
+
+exports.writePage = writePage;
+exports.getPage = getPage;
+exports.init = init;
