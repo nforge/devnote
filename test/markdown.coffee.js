c375b90a943bf91612b2b4cf9485e02588d73447
Yi, EungJun <semtlenori@gmail.com>
2012-04-12 12:09:13 +0900

    Remove tests for markdown.

--- test/markdown.coffee.js
+++
@@ -1,8 +0,0 @@
-assert = require 'assert'
-md = require 'markdown-js'
-gfm = require '../gfm'
-
-suite 'Markdown', () ->
-    test 'newline', () ->
-        source = 'Roses are red\nViolets are blue'
-        md.parse gfm.toMarkdown source
