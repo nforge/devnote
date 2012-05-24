297d43aa0b5c219787c3ff3b0c6f54a8d1bc1551
Yi, EungJun <semtlenori@gmail.com>
2012-05-24 14:20:58 +0900

    app: Apply codemirror2.
    * Import codemirror2 from http://marijnhaverbeke.nl/git/codemirror2
    * Apply codemirror2 to new and edit page.
    * Make preview.js and draft.js compatible with codemirror2.
    * Minor fix to keep pace with renaming functions of keynavi.js.
      Actually, it has nothing to do with codemirror2.

--- public/scripts/draft.js
+++ public/scripts/draft.js
@@ -1,8 +1,10 @@
+var draft = {}
+
 var _key = function(name) {
     return prefix + '-' + name;
 }
 
-var existUnsavedDocument = function(fields) {
+var existDraft = function(fields) {
     for(var i in fields) {
         if (localStorage.getItem(_key(fields[i]))) {
             return true;
@@ -12,20 +14,35 @@ var existUnsavedDocument = function(fields) {
     return false;
 }
 
-var restoreUnsavedDocument = function(fields) {
+var _callbackAfterRestoreDraft = function() {}
+
+var restoreDraft = function(fields) {
     for(var i in fields) {
-        var value = localStorage.getItem(_key(fields[i]));
+        var field = fields[i];
+        var value = localStorage.getItem(_key(field));
         if (value) {
-            $(fields[i]).val(value);
+            if (typeof(field) == 'object' && field.setValue) {
+                field.setValue(value);
+            } else {
+                $(field).val(value);
+            }
         }
     }
+
+    _callbackAfterRestoreDraft();
 }
 
 var bindFormAndLocalStorage = function(form, fields) {
     var _bind = function(field) {
-        $(field).keyup(function() {
-            localStorage.setItem(_key(field), $(field).val());
-        });
+        if (typeof(field) == 'object' && field.getValue) {
+            field.setOption('onKeyEvent', function() {
+                localStorage.setItem(_key(field), field.getValue());
+            });
+        } else {
+            $(field).keyup(function() {
+                localStorage.setItem(_key(field), $(field).val());
+            });
+        }
     }
 
     for(var i in fields) {
@@ -39,7 +56,11 @@ var bindFormAndLocalStorage = function(form, fields) {
     });
 }
 
-var initDraft = function(_prefix, form, fields) {
+draft.onRestore = function(callback) {
+    _callbackAfterRestoreDraft = callback;
+}
+
+draft.init = function(_prefix, form, fields) {
     if (typeof(localStorage) != 'object') {
         return false;
     }
@@ -48,18 +69,21 @@ var initDraft = function(_prefix, form, fields) {
         prefix = _prefix;
 
         // notifaction message
-        if (existUnsavedDocument(fields)) {
-            $('<div id="draft-message"></div>')
+        if (existDraft(fields)) {
+            var restoreLink = 
+                $('<a id="restore-draft">Click here</a>')
+                .click(function () {
+                    restoreDraft(fields);
+                    $('#draft-message').css("display", "none");
+                });
+
+            draft.notification =
+                $('<div id="draft-message"></div>')
                 .addClass('alert').addClass('alert-info')
                 .append('<span>Unsaved draft exists. </span>')
-                .append('<a id="restore-draft">Click here</a>')
+                .append(restoreLink)
                 .append('<span> to recover it.</span>')
                 .insertBefore($(form));
-
-            $('#restore-draft').click(function () {
-                restoreUnsavedDocument(fields);
-                $('#draft-message').css("display", "none");
-            });
         }
 
         bindFormAndLocalStorage(form, fields);
--- public/scripts/preview.js
+++ public/scripts/preview.js
@@ -1,25 +1,31 @@
+var preview = {
+    last_text: '',
+    converter: new Showdown.converter(),
+}
 
-    var last_text = '';
-    var converter = new Showdown.converter();
-    var preview = function() {
-        var text = $("#body").val();
+preview.update = function() {
+    var text = preview.getText();
 
-        text = text.replace(/```(\w+)((\r|\n|.)*?)(\r|\n)```/gm, function(match, p1, p2) {
-            try {
-                return '<pre><code class="' + p1 + '">' + hljs(p2, p1).value + '</code></pre>';
-            } catch(e) {
-                return '<pre><code>' + hljs(p2).value + '</code></pre>';
-            }
-        });
-
-        if (last_text != text) {
-            var html = converter.makeHtml(text);
-            $("#preview").html(html);
-            last_text = text;
+    text = text.replace(/```(\w+)((\r|\n|.)*?)(\r|\n)```/gm, function(match, p1, p2) {
+        try {
+            return '<pre><code class="' + p1 + '">' + hljs(p2, p1).value + '</code></pre>';
+        } catch(e) {
+            return '<pre><code>' + hljs(p2).value + '</code></pre>';
         }
-    }
+    });
 
-$(document).ready(function init() {
-    $("#body").keyup(preview);
-    preview();
-});
+    if (preview.last_text != text) {
+        var html = preview.converter.makeHtml(text);
+        $("#preview").html(html);
+        preview.last_text = text;
+    }
+}
+
+preview.init = function(editorElement, getText) {
+    preview.getText = getText;
+
+    $(document).ready(function() {
+        editorElement.keyup(preview.update);
+        preview.update();
+    });
+}
