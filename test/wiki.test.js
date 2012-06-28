788ad573f700f2daa4adaa068a4b734c52fef82d
Yi EungJun <semtlenori@gmail.com>
2012-06-28 11:50:49 +0900

    renderer: Extract tests for renderer from wiki.test.js.

--- test/wiki.test.js
+++ test/wiki.test.js
@@ -33,16 +33,6 @@ suite('wiki', function() {
     });
   });
 
-  test('사용자는 마크다운 포맷으로 작성된 위키페이지를 렌더링된 HTML 페이지로 볼 수 있다.', function() {
-    assert.equal(wiki.render('Welcome to **n4wiki**'), '<p>Welcome to <strong>n4wiki</strong></p>');
-  });
-
-  test('사용자는 소스코드가 포함된 위키페이지를 구문강조된 HTML 페이지로 볼 수 있다.', function() {
-    var actual = wiki.render("```python\ndef foo():\n  print 'bar'\n```");
-    var expected = '<pre><code class="python">\n<span class="function"><span class="keyword">def</span> <span class="title">foo</span><span class="params">()</span>:</span>\n  <span class="keyword">print</span> <span class="string">\'bar\'</span></code></pre>'
-    assert.equal(actual, expected)
-  });
-
   test('사용자는 위키 페이지를 삭제할 수 있다.', function(done) {
     var name = 'my diary';
     var content = 'It\'s a very busy today';
