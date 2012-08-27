nforge/devnote

commit d22edbb92d45833b9cef6145d46755570c0515c5

wiki: #3, #6 Use logged user as author.
While edit, delete or rename a page, use logged user as the author
of the change.

views/admin/adduser.jade
@@ -4,28 +4,33 @@ block prepend nav
   selected = 'adduser'
 
 block content
-    form(method="post", action="/wikis/note/users/new").span9.form-horizontal
-      div.page-header
-        h1= __('New user')
+  form(method="post", action="/wikis/note/users/new").span9.form-horizontal
+    div.page-header
+      h1= __('New user')
 
-      div.control-group
-        label(for="name").control-label= __('User name')
-        div.controls
-          input(type="text", name="name", id="name", required)
+    div.control-group
+      label(for="name").control-label= __('User name')
+      div.controls
+        input(type="text", name="name", id="name", required)
 
-      div.control-group
-        label(for="id").control-label= __('Id')
-        div.controls
-          input(type="text", name="id", id="id", required)
+    div.control-group
+      label(for="id").control-label= __('Id')
+      div.controls
+        input(type="text", name="id", id="id", required)
 
-      div.control-group
-        label(for="email").control-label= __('Email')
-        div.controls
-          input(type="text", name="email", id="email", required)
+    div.control-group
+      label(for="email").control-label= __('Email')
+      div.controls
+        input(type="text", name="email", id="email", required)
 
-      div.control-group
-        label(for="password").control-label= __('Password')
-        div.controls
-          input(type="password", name="password", id="password", placeholder=__("At least 4 characters in length."), required)
+    div.control-group
+      label(for="timezone").control-label= __('Timezone')
+      div.controls
+        input(type="text", name="timezone", id="timezone", value="#{user.timezone}")
 
-      button(type="submit").btn.btn-primary: strong= __('Save')
+    div.control-group
+      label(for="password").control-label= __('Password')
+      div.controls
+        input(type="password", name="password", id="password", placeholder=__("At least 4 characters in length."), required)
+
+    button(type="submit").btn.btn-primary: strong= __('Save')

lib/users.js
@@ -21,6 +21,11 @@ var User = function() {
   var users = {};
   var init = function() {
     users = config.get('users') || {};
+    for (var key in users) {
+      if (!users[key].timezone) {
+        users[key].timezone = '+0900';
+      }
+    }
   }
 
   var add = function(user) {

lib/wiki.js
@@ -20,18 +20,21 @@ var init = function(wikiname, callback) {
  * @param {String} content
  * @param {String} message commit message
  */
-var _createCommitRequest = function(name, content, message) {
-  var files = {};
-  var writer = {
-    name: 'Guest',
-    mail: 'guest@n4wiki.com',
-    timezone: '+0900'
-  };
+var _createCommitRequest = function(name, content, user, message) {
+  var files = {}, writer;
+
+  if (!user) {
+    user = {
+      name: 'Guest',
+      email: 'guest@n4wiki.com',
+      timzeon: '+0900'
+    };
+  }
 
   files[name.toString()] = content;
   return {
     files: files,
-    user: writer,
+    user: user,
     message: message
   };
 };
@@ -97,7 +100,7 @@ var getPage = function(pagename, commitId_, callback) {
  * @param {String} content
  * @param {Function} callback (err, commit)
  */
-var writePage = function(name, content, callback) {
+var writePage = function(name, content, user, callback) {
   getPage(name, function(err, page) {
     var getLineEnding, lineEnding, request;
 
@@ -150,11 +153,21 @@ var writePage = function(name, content, callback) {
       return callback();
     }
 
-    request = _createCommitRequest(name, content, 'Edit ' + name);
+    request = _createCommitRequest(name, content, user, 'Edit ' + name);
 
     return gitfs.commit(request, callback);
   });
 };
+
+var _getAuthorSign = function(user) {
+  var unixtime = Math.round(new Date().getTime() / 1000);
+
+  if (user) {
+    return user.name + ' <' + user.email + '> ' + unixtime + ' ' + user.timezone;
+  } else {
+    return 'Guest <guest@n4wiki.com> ' + unixtime + ' +0900';
+  }
+}
 
 /**
  * Rename a page.
@@ -163,17 +176,17 @@ var writePage = function(name, content, callback) {
  * @param {String} to
  * @param {Function} callback (err, commit)
  */
-var renamePage = function(from, to, callback) {
+var renamePage = function(from, to, user, callback) {
   var renameAndCommit = function(baseTree, from, to, callback) {
     gitfs.getCommitIdFromHEAD(function(err, HEAD) {
-      var unixtime, newTree;
+      var newTree, author;
 
-      unixtime = Math.round(new Date().getTime() / 1000);
+      author = _getAuthorSign(user);
       var commitData = {
         parent: HEAD,
         files: {},
-        author: 'Guest <guest@n4wiki.com> ' + unixtime + ' +0900',
-        committer: 'Guest <guest@n4wiki.com> ' + unixtime + ' +0900',
+        author: author,
+        committer: author,
         message: 'Rename: ' + from + ' => ' + to
       };
 
@@ -202,17 +215,17 @@ var renamePage = function(from, to, callback) {
  * @param {String} pagename
  * @param {Function} callback (err, commit)
  */
-var deletePage = function(pagename, callback) {
+var deletePage = function(pagename, user, callback) {
   var deleteAndCommit = function(baseTree, pagename, callback) {
     gitfs.getCommitIdFromHEAD(function(err, HEAD) {
-      var unixtime, newTree;
+      var newTree, author;
 
-      unixtime = Math.round(new Date().getTime() / 1000);
+      author = _getAuthorSign(user);
       var commitData = {
         parent: HEAD,
         files: {},
-        author: 'Guest <guest@n4wiki.com> ' + unixtime + ' +0900',
-        committer: 'Guest <guest@n4wiki.com> ' + unixtime + ' +0900',
+        author: author,
+        committer: author,
         message: 'Delete ' + pagename
       };
 

locales/ko.js
@@ -104,5 +104,9 @@
 	"B": "B",
 	"greetings": "greetings",
 	"welcome greetings": "만나서 반갑습니다.",
-	"Admin": "관리"
+	"Admin": "관리",
+	"Timezone": "시간대",
+	"Current password": "현재 비밀번호",
+	"New password": "새 비밀번호",
+	"Edit user": "사용자 설정 변경"
 }

views/admin/edituser.jade
@@ -1,17 +1,41 @@
-extends ../layout
+extends adminLayout
+
+block prepend nav
+  selected = 'userlist'
 
 block content
-  h1= title
-  form(method="post", action="/wikis/note/user/#{user.id}")
-    label(for="name") 이름
-    input(name="name", id="name", value="#{user.name}", disabled).uneditable-input
-    label(for="id") id
-    input(name="id", id="id", value="#{user.id}", disabled).uneditable-input
-    label(for="email") email
-    input(name="email", id="email", value="#{user.email}" )
-    label(for="previousPassword") 이전 패스워드
-    input(name="previousPassword", type="password", id="previousPassword")
-    label(for="newPassword") 신규 패스워드
-    input(name="newPassword", type="password", id="newPassword", placeholder="4자리 이상으로 입력해 주세요.")
-    br
-    input(type="submit", name="submit").btn-primary
+  form(method="post", action="/wikis/note/user/#{user.id}").span9.form-horizontal
+    div.page-header
+      h1= __('Edit user')
+
+    div.control-group
+      label(for="name").control-label= __('Name')
+      div.controls
+        input(type="text", name="name", id="name", value="#{user.name}", disabled).uneditable-input
+
+    div.control-group
+      label(for="id").control-label= __('Id')
+      div.controls
+        input(type="text", name="id", id="id", value="#{user.id}", disabled).uneditable-input
+
+    div.control-group
+      label(for="email").control-label= __('Email')
+      div.controls
+        input(type="text", name="email", id="email", value="#{user.email}", required)
+
+    div.control-group
+      label(for="timezone").control-label= __('Timezone')
+      div.controls
+        input(type="text", name="timezone", id="timezone", value="#{user.timezone}")
+
+    div.control-group
+      label(for="previousPassword").control-label= __('Current password')
+      div.controls
+        input(type="password", name="previousPassword", id="previousPassword")
+
+    div.control-group
+      label(for="newPassword").control-label= __('New password')
+      div.controls
+        input(type="password", name="newPassword", id="newPassword", placeholder="4자리 이상으로 입력해 주세요.")
+
+    button(type="submit").btn.btn-primary: strong= __('Save')

wikiApp.coffee
@@ -23,7 +23,7 @@ exports.init = (wikiname) ->
       console.log err.message
     else
       data = fs.readFileSync 'frontpage.md'
-      wiki.writePage 'frontpage', data, (err) ->
+      wiki.writePage 'frontpage', data, null, (err) ->
         throw err if err
 
 error404 = (err, req, res, next) ->
@@ -192,7 +192,7 @@ exports.getNew = (req, res) ->
 
 exports.postNew = (req, res) ->
   saveEditedPage = (name, body, callback) ->
-    wiki.writePage name, body, (err, commitId) ->
+    wiki.writePage name, body, req.session.user, (err, commitId) ->
       if req.session.user
         userId = req.session.user.id
         if not lastVisits[userId]
@@ -227,7 +227,7 @@ exports.postNew = (req, res) ->
   body = req.body.body
 
   if originalPageName and (originalPageName != newPageName)
-    wiki.renamePage originalPageName, newPageName, (err) ->
+    wiki.renamePage originalPageName, newPageName, req.session.user, (err) ->
       saveEditedPage newPageName, body, (err) ->
         res.redirect ROOT_PATH + '/pages/' + encodeURIComponent(newPageName)
   else
@@ -235,7 +235,7 @@ exports.postNew = (req, res) ->
       res.redirect ROOT_PATH + '/pages/' + encodeURIComponent(newPageName)
 
 exports.postDelete = (req, res) ->
-  wiki.deletePage req.params.name, (err) ->
+  wiki.deletePage req.params.name, req.session.user, (err) ->
     res.redirect ROOT_PATH + '/pages?deletedPageName=' + req.params.name
 
 exports.postRollback = (req, res) ->

commit 6e55abb2a879cfe94c520b1cb7ca202244748709

user: Fix an error on Add User page.
This error is caused by new timezone property introduced in d22edbb.

userApp.coffee
@@ -54,6 +54,7 @@ exports.postNew = (req, res) ->
     id: req.body.id,
     name: req.body.name,
     email: req.body.email,
+    timezone: req.body.timezone,
     password: req.body.password
   userInfo = User.findUserById req.body.id
 

views/admin/adduser.jade
@@ -26,7 +26,7 @@ block content
     div.control-group
       label(for="timezone").control-label= __('Timezone')
       div.controls
-        input(type="text", name="timezone", id="timezone", value="#{user.timezone}")
+        input(type="text", name="timezone", id="timezone")
 
     div.control-group
       label(for="password").control-label= __('Password')